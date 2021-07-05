669b3516227a6d2096f33331a576a2470ca7b60f
Daniel Mason <danmasson0@gmail.com>
2021-07-05 11:30:49 -0400

    changed numberOfToppings

--- homeWorkWeek2.js
+++ homeWorkWeek2.js
@@ -1,2 +1,10 @@
-pizzaPlace = Dominos;
-numberOfToppings = 2;
+let pizzaPlace = "Dominos";
+let numberOfToppings = 0;
+
+console.log(pizzaPlace, typeof pizzaPlace);
+console.log(numberOfToppings, typeof numberOfToppings);
+console.log(`The pizza from ${pizzaPlace} is good though you're better off with getting just ${numberOfToppings} toppings.`);
+
+if (numberOfToppings <= 10) {
+    console.log("Quality, not quantity")
+}
