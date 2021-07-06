4b402b92cfa948423e1eb3973dc1ac6ab4d6826d
Daniel Mason <danmasson0@gmail.com>
2021-07-05 23:19:54 -0400

    final change for if statement

--- homeWorkWeek2.js
+++ homeWorkWeek2.js
@@ -5,7 +5,7 @@ console.log(pizzaPlace, typeof pizzaPlace);
 console.log(numberOfToppings, typeof numberOfToppings);
 console.log(`The pizza from ${pizzaPlace} is good though you're better off with getting just ${numberOfToppings} toppings.`);
 
-if (numberOfToppings <= 10) {
+if (numberOfToppings < 10) {
     console.log("Quality, not quantity");
 } else {
     console.log("A whole lot of pizza");
