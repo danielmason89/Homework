82f5e0c203df4a589cbf27685223cdd8801c46b9
Daniel Mason <danmasson0@gmail.com>
2021-07-05 23:07:47 -0400

    added semi-colon

--- homeWorkWeek2.js
+++ homeWorkWeek2.js
@@ -6,10 +6,10 @@ console.log(numberOfToppings, typeof numberOfToppings);
 console.log(`The pizza from ${pizzaPlace} is good though you're better off with getting just ${numberOfToppings} toppings.`);
 
 if (numberOfToppings <= 10) {
-    console.log("Quality, not quantity")
+    console.log("Quality, not quantity");
 };
 if (numberOfToppings >= 10) {
-    console.log("A whole lot of pizza")
+    console.log("A whole lot of pizza");
 };
 
 for (let numberOfToppings = 1; numberOfToppings <= 10; numberOfToppings++) {
