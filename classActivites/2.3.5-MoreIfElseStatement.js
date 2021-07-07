66fad58b87e313486136456b6bad10373ebc1aae
Daniel Mason <danmasson0@gmail.com>
2021-07-06 21:50:34 -0400

    updated answer to 2.3.5

---
+++ classActivites/2.3.5-MoreIfElseStatement.js
@@ -0,0 +1,12 @@
+let num = 10;
+if (num > 100) {
+    console.log("This number is greater than 100.");
+} else if (num <= 40) {
+    console.log("The number is less than or equal to 40.");
+} else if (num === 42) {
+    console.log("The number is 42.")
+} else if (num >= 45) {
+    console.log("The number is greater than or equal to 45.")
+} else {
+    console.log("The number is one of these three numbers:,,.")
+};
