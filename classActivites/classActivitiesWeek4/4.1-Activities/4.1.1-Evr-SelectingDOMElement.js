099a68410acd21156b46d3592677c31479baf982
Daniel Mason <danmasson0@gmail.com>
2021-07-13 22:38:54 -0400

    finished most of activities

---
+++ classActivites/classActivitiesWeek4/4.1-Activities/4.1.1-Evr-SelectingDOMElement.js
@@ -0,0 +1,8 @@
+// select all unordered list (ul) elements
+console.log(document.getElementsByTagName('ul'));
+// select all elements with the class "class-week"
+console.log(document.getElementByClassName("class-week"));
+// select all elements with the class "nav-bar"
+
+// select the element with the id "dog-picture" and save it to a variable
+document.querySelector("img").src = "https://i2.pickpik.com/photos/774/556/1024/odin-male-australian-shepherd-dog-thumb.jpg";
